a38a20097d3bcd2635d855fc0494a6769cf4b958
camilabustosv <bustos.valverde.camila@gmail.com>
2021-09-10 11:52:03 -0400

    Clase 3: Dino

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from nlc_dino_runner.components.dinosaur import Dinosaur
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_WIDTH, SCREEN_HEIGHT, BG, FPS
 
 
@@ -12,8 +13,9 @@ class Game:
        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
        self.playing = False
        self.x_pos_bg = 0
-       self.y_pos_bg = 360
+       self.y_pos_bg = 380
        self.game_speed = 20
+       self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -29,13 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
-
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -43,12 +46,8 @@ class Game:
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
 
-
         self.screen.blit(BG, (self.x_pos_bg + image_width, self.y_pos_bg))
-
 
         if self.x_pos_bg <= -image_width:
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
-
-
--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -4,5 +4,3 @@ from nlc_dino_runner.components.game import Game
 if __name__ == "__main__":
     game = Game()
     game.run()
-
-
